9fbcac3587c672f85e6068c61c50445c1f9de329
YongAn <414675056@qq.com>
2022-09-24 00:06:37 +0800

    1

--- demoForConsoler31/Program.cs
+++ demoForConsoler31/Program.cs
@@ -10,7 +10,7 @@ namespace demoForConsoler31
             // 创建配置构建器
             IConfigurationBuilder builder = new ConfigurationBuilder();
             // 添加Json文件数据源
-            builder.AddJsonFile("appsettings.json");
+            builder.AddJsonFile("appsettings2.json", optional:true);
             // 构建配置获取配置根
             IConfigurationRoot configurationRoot = builder.Build();
             // 获取并输出配置根下的值
@@ -18,6 +18,7 @@ namespace demoForConsoler31
             Console.WriteLine(configurationRoot["car2"]);
             Console.WriteLine(configurationRoot["car3"]);
             Console.WriteLine(configurationRoot["car4"]);
+            Console.WriteLine(configurationRoot["car5"]);
         }
     }
 }
