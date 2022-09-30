e795a500cc7feae3297851f28ea0944fb6128280
YongAn <414675056@qq.com>
2022-10-01 01:02:11 +0800

    1

---
+++ demoForConsoler31/Program2.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+
+namespace demoForConsoler31
+{
+    //internal class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        // 创建配置构建器
+    //        IConfigurationBuilder builder = new ConfigurationBuilder();
+    //        // 添加Ini文件数据源
+    //        builder.AddIniFile("appsettings.ini");
+    //        // 构建配置获取配置根
+    //        IConfigurationRoot configurationRoot = builder.Build();
+    //        // 获取并输出配置根下的值
+    //        Console.WriteLine(configurationRoot["tesla"]);
+    //        Console.ReadKey();
+    //    }
+    //}
+}
--- demoForConsoler31/Program3.cs
+++ demoForConsoler31/Program3.cs
@@ -11,16 +11,10 @@ namespace demoForConsoler31
     //        IConfigurationBuilder builder = new ConfigurationBuilder();
     //        // 添加Json文件数据源
     //        builder.AddJsonFile("appsettings.json");
+    //        // 添加Json文件数据源
+    //        builder.AddJsonFile("appsettings.dev.json");
     //        // 构建配置获取配置根
     //        IConfigurationRoot configurationRoot = builder.Build();
-    //        // 获取并输出配置根下的值
-    //        Console.WriteLine(configurationRoot["car1"]);
-    //        Console.WriteLine(configurationRoot["car2"]);
-    //        Console.WriteLine(configurationRoot["car3"]);
-    //        Console.WriteLine(configurationRoot["car4"]);
-    //        Console.WriteLine(configurationRoot["car5"]);
-    //        Console.ReadKey();
-
     //        // 获取并输出配置根下的值
     //        Console.WriteLine(configurationRoot["car1"]);
     //        Console.WriteLine(configurationRoot["car2"]);
